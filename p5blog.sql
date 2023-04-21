0ad50400ae33f38d106a421f4b08de2364204163
itopi-fr <88286107+itopi-fr@users.noreply.github.com>
2023-04-21 16:54:19 +0200

    Updated database

--- p5blog.sql
+++ p5blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 20 avr. 2023 à 13:19
+-- Généré le : ven. 21 avr. 2023 à 14:52
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.13
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS comment;
-CREATE TABLE IF NOT EXISTS `comment` (
+CREATE TABLE IF NOT EXISTS comment (
   id int NOT NULL AUTO_INCREMENT,
   post_id int NOT NULL,
   author_id int NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `comment` (
 --
 
 DROP TABLE IF EXISTS file;
-CREATE TABLE IF NOT EXISTS `file` (
+CREATE TABLE IF NOT EXISTS file (
   id int NOT NULL AUTO_INCREMENT,
   title varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   url varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `file` (
   mime varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   size int NOT NULL,
   PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=161 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=177 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table file
@@ -64,11 +64,11 @@ CREATE TABLE IF NOT EXISTS `file` (
 INSERT INTO file (id, title, url, ext, mime, size) VALUES
 (1, 'default-avatar', '/public/assets/img/default-avatar.jpg', 'jpg', 'image/jpeg', 20),
 (2, 'default-post', '/public/assets/img/default-post.jpg', 'jpg', 'image/jpeg', 693),
-(147, 'vide.pdf', '/public/upload/owner/vide_b0403b.pdf', 'pdf', 'application/pdf', 27706),
-(148, 'kara_small.jpg', '/public/upload/owner/kara_small_110e7d.jpg', 'jpg', 'image/jpeg', 8111),
 (157, 'test.jpg', '/public/upload/blog/post/test_97cac7.jpg', 'jpg', 'image/jpeg', 693),
-(158, 'default-post.jpg', '/public/upload/blog/post/default-post_1271a2.jpg', 'jpg', 'image/jpeg', 11571),
-(160, 'kara_small.jpg', '/public/upload/user/kara_small_8107c8.jpg', 'jpg', 'image/jpeg', 8111);
+(161, 'test.jpg', '/public/upload/blog/post/test_a98bc6.jpg', 'jpg', 'image/jpeg', 693),
+(172, 'kara_small.jpg', '/public/upload/user/kara_small_4d30bc.jpg', 'jpg', 'image/jpeg', 8111),
+(173, 'vide.pdf', '/public/upload/owner/vide_bc36d2.pdf', 'pdf', 'application/pdf', 27706),
+(174, 'test.jpg', '/public/upload/owner/test_1981d3.jpg', 'jpg', 'image/jpeg', 693);
 
 -- --------------------------------------------------------
 
@@ -91,17 +91,17 @@ CREATE TABLE IF NOT EXISTS post (
   PRIMARY KEY (id),
   KEY author_id (author_id) USING BTREE,
   KEY feat_img_id (feat_img_id) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table post
 --
 
 INSERT INTO post (id, author_id, feat_img_id, title, slug, excerpt, content, creation_date, last_update, status) VALUES
-(23, 94, 2, 'Nam viverra commodo lacusd', 'nam-viverra-commodo-lacus', 'Nam viverra commodo lacus. Vestibulum ante ipsum', 'Nam viverra commodo lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris ac est sit amet arcu laoreet pellentesque. Integer sollicitudin, dui et tincidunt fermentum, velit purus lobortis metus, vitae tincidunt leo dui at enim. Vestibulum lacus lacus, hendrerit sed egestas vel, sodales vel nibh. Etiam condimentum finibus massa maximus dignissim. Etiam pharetra, mauris sed gravida fermentum, est lacus rutrum augue, eget fermentum tellus urna et ipsum. Donec iaculis, nisi sit amet semper sodales, turpis nisl blandit orci, vulputate cursus mauris diam ac lorem. Duis facilisis est eget fringilla maximus. Suspendisse sit amet libero auctor, pulvinar nulla id, porta nisi.\r\n\r\nFusce sollicitudin lorem a euismod ultrices. Quisque semper viverra sem, sit amet rutrum urna varius nec. Praesent ac turpis pretium, dictum lorem eu, sagittis ex. Donec rhoncus pellentesque metus eu vehicula. Maecenas sit amet est eu augue blandit pellentesque eget vitae lacus. Donec hendrerit nisl ut sapien pulvinar ultrices. Proin ex purus, gravida ut mauris nec, bibendum efficitur leo. Quisque placerat porta tortor quis dictum. Maecenas nec metus non felis maximus ultrices in vel risus. Nunc ultricies ut ex ut ultrices. Nunc quis tortor vitae ex cursus ornare sed ac tortor.\r\n\r\n', '2023-04-06 10:39:39', '2023-04-20 08:48:30', 'draft'),
-(25, 94, 2, 'Maecenas feugiat interdum quam', 'maecenas-feugiat-interdum-quam', 'Maecenas feugiat interdum quam, vel dictum mauris elementum at.', 'Maecenas feugiat interdum quam, vel dictum mauris elementum at. Sed ut elementum sem. Mauris in semper tortor. Mauris ante tortor, tincidunt eget rhoncus et, venenatis eget nibh. Aliquam fermentum aliquet magna, a blandit tellus dignissim egestas. Morbi fermentum ornare blandit. Cras gravida lacinia semper. Sed venenatis ipsum ac quam suscipit pulvinar. Praesent blandit, augue et condimentum fringilla, magna nisi sagittis tellus, in posuere ipsum purus vel sem. Proin molestie ligula ex, quis porta ligula porttitor id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque vehicula quam vel dapibus mollis. Sed luctus sed odio sed pharetra. Suspendisse finibus dui vitae sapien consequat, nec ultrices magna convallis. Vestibulum convallis nulla sed sapien vehicula, vitae euismod nibh aliquam.\r\n\r\nFusce sollicitudin lorem a euismod ultrices. Quisque semper viverra sem, sit amet rutrum urna varius nec. Praesent ac turpis pretium, dictum lorem eu, sagittis ex. Donec rhoncus pellentesque metus eu vehicula. Maecenas sit amet est eu augue blandit pellentesque eget vitae lacus. Donec hendrerit nisl ut sapien pulvinar ultrices. Proin ex purus, gravida ut mauris nec, bibendum efficitur leo. Quisque placerat porta tortor quis dictum. Maecenas nec metus non felis maximus ultrices in vel risus. Nunc ultricies ut ex ut ultrices. Nunc quis tortor vitae ex cursus ornare sed ac tortor.\r\n\r\n\r\n\r\n', '2023-03-02 10:39:39', '2023-04-13 10:39:39', 'arch'),
-(26, 94, 157, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem', 'sed-ut-perspiciatis-unde-omnis-iste-natus', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor...', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', '2023-04-20 12:51:50', '2023-04-20 12:51:50', 'pub'),
-(27, 94, 158, 'Nouvel articleu', 'nouvel-article', 'sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. N...', 'sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', '2023-04-20 12:56:43', '2023-04-20 13:13:58', 'pub');
+(23, 94, 2, 'Nam viverra commodo lacusd', 'nam-viverra-commodo-lacus', 'Nam viverra commodo lacus. Vestibulum ante ipsum', 'Nam viverra commodo lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris ac est sit amet arcu laoreet pellentesque. Integer sollicitudin, dui et tincidunt fermentum, velit purus lobortis metus, vitae tincidunt leo dui at enim. Vestibulum lacus lacus, hendrerit sed egestas vel, sodales vel nibh. Etiam condimentum finibus massa maximus dignissim. Etiam pharetra, mauris sed gravida fermentum, est lacus rutrum augue, eget fermentum tellus urna et ipsum. Donec iaculis, nisi sit amet semper sodales, turpis nisl blandit orci, vulputate cursus mauris diam ac lorem. Duis facilisis est eget fringilla maximus. Suspendisse sit amet libero auctor, pulvinar nulla id, porta nisi.\r\n\r\nFusce sollicitudin lorem a euismod ultrices. Quisque semper viverra sem, sit amet rutrum urna varius nec. Praesent ac turpis pretium, dictum lorem eu, sagittis ex. Donec rhoncus pellentesque metus eu vehicula. Maecenas sit amet est eu augue blandit pellentesque eget vitae lacus. Donec hendrerit nisl ut sapien pulvinar ultrices. Proin ex purus, gravida ut mauris nec, bibendum efficitur leo. Quisque placerat porta tortor quis dictum. Maecenas nec metus non felis maximus ultrices in vel risus. Nunc ultricies ut ex ut ultrices. Nunc quis tortor vitae ex cursus ornare sed ac tortor.\r\n\r\n', '2023-04-06 10:39:39', '2023-04-21 14:40:33', 'pub'),
+(25, 94, 2, 'Maecenas feugiat interdum quam', 'maecenas-feugiat-interdum-quam', 'Maecenas feugiat interdum quam, vel dictum mauris elementum at.', 'Maecenas feugiat interdum quam, vel dictum mauris elementum at. Sed ut elementum sem. Mauris in semper tortor. Mauris ante tortor, tincidunt eget rhoncus et, venenatis eget nibh. Aliquam fermentum aliquet magna, a blandit tellus dignissim egestas. Morbi fermentum ornare blandit. Cras gravida lacinia semper. Sed venenatis ipsum ac quam suscipit pulvinar. Praesent blandit, augue et condimentum fringilla, magna nisi sagittis tellus, in posuere ipsum purus vel sem. Proin molestie ligula ex, quis porta ligula porttitor id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque vehicula quam vel dapibus mollis. Sed luctus sed odio sed pharetra. Suspendisse finibus dui vitae sapien consequat, nec ultrices magna convallis. Vestibulum convallis nulla sed sapien vehicula, vitae euismod nibh aliquam.\r\n\r\nFusce sollicitudin lorem a euismod ultrices. Quisque semper viverra sem, sit amet rutrum urna varius nec. Praesent ac turpis pretium, dictum lorem eu, sagittis ex. Donec rhoncus pellentesque metus eu vehicula. Maecenas sit amet est eu augue blandit pellentesque eget vitae lacus. Donec hendrerit nisl ut sapien pulvinar ultrices. Proin ex purus, gravida ut mauris nec, bibendum efficitur leo. Quisque placerat porta tortor quis dictum. Maecenas nec metus non felis maximus ultrices in vel risus. Nunc ultricies ut ex ut ultrices. Nunc quis tortor vitae ex cursus ornare sed ac tortor.\r\n\r\n\r\n\r\n', '2023-03-02 10:39:39', '2023-04-21 14:40:41', 'pub'),
+(26, 94, 157, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem', 'sed-ut-perspiciatis-unde-omnis-iste-natus', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor...', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', '2023-04-20 12:51:50', '2023-04-20 16:07:57', 'draft'),
+(29, 94, 161, 'Titre de l\'article', 'slug-article', 'test...', 'test', '2023-04-20 13:37:51', '2023-04-20 16:07:46', 'arch');
 
 -- --------------------------------------------------------
 
@@ -135,7 +135,7 @@ CREATE TABLE IF NOT EXISTS token (
   type varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (id),
   KEY fk_token_user1_idx (user_id)
-) ENGINE=InnoDB AUTO_INCREMENT=150 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=154 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -144,7 +144,7 @@ CREATE TABLE IF NOT EXISTS token (
 --
 
 DROP TABLE IF EXISTS user;
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS user (
   id int NOT NULL AUTO_INCREMENT,
   avatar_id int DEFAULT NULL,
   pseudo varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -153,15 +153,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   role varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (id),
   KEY file_id (avatar_id) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=117 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table user
 --
 
 INSERT INTO user (id, avatar_id, pseudo, pass, email, role) VALUES
-(94, 160, 'owner', 'dd87c23f8d5c9e464d4d4183e9522f614093e9eff6675c22e5bc150c4b82bfca', 'md@itopi.fr', 'owner'),
-(116, NULL, 'newuser', '158f1aab96e118a27497308a1c6e15100da44840f31d5456e1042feb29a9c8ec', 'pub@itopi.fr', 'user');
+(94, 172, 'owner', 'dd87c23f8d5c9e464d4d4183e9522f614093e9eff6675c22e5bc150c4b82bfca', 'md@itopi.fr', 'owner'),
+(116, NULL, 'newuser', 'dd87c23f8d5c9e464d4d4183e9522f614093e9eff6675c22e5bc150c4b82bfca', 'pub@itopi.fr', 'user');
 
 -- --------------------------------------------------------
 
@@ -189,7 +189,7 @@ CREATE TABLE IF NOT EXISTS user_owner_infos (
 --
 
 INSERT INTO user_owner_infos (owner_id, user_id, photo_file_id, cv_file_id, first_name, last_name, catch_phrase) VALUES
-(3, 94, 148, 147, 'Maxime', 'Hello Hello', 'Hello Catch Phrase yop');
+(3, 94, 174, 173, 'Maxime', 'Hello Hello', 'Hello Catch Phrase yop');
 
 --
 -- Contraintes pour les tables déchargées
@@ -200,34 +200,34 @@ INSERT INTO user_owner_infos (owner_id, user_id, photo_file_id, cv_file_id, firs
 --
 ALTER TABLE comment
   ADD CONSTRAINT fk_comment_post1 FOREIGN KEY (post_id) REFERENCES post (id),
-  ADD CONSTRAINT fk_comment_user1 FOREIGN KEY (author_id) REFERENCES `user` (id);
+  ADD CONSTRAINT fk_comment_user1 FOREIGN KEY (author_id) REFERENCES user (id);
 
 --
 -- Contraintes pour la table post
 --
 ALTER TABLE post
-  ADD CONSTRAINT post_ibfk_1 FOREIGN KEY (feat_img_id) REFERENCES `file` (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT post_ibfk_2 FOREIGN KEY (author_id) REFERENCES `user` (id) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT post_ibfk_1 FOREIGN KEY (feat_img_id) REFERENCES file (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT post_ibfk_2 FOREIGN KEY (author_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table token
 --
 ALTER TABLE token
-  ADD CONSTRAINT fk_token_user1 FOREIGN KEY (user_id) REFERENCES `user` (id);
+  ADD CONSTRAINT fk_token_user1 FOREIGN KEY (user_id) REFERENCES user (id);
 
 --
 -- Contraintes pour la table user
 --
 ALTER TABLE user
-  ADD CONSTRAINT user_ibfk_2 FOREIGN KEY (avatar_id) REFERENCES `file` (id) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD CONSTRAINT user_ibfk_2 FOREIGN KEY (avatar_id) REFERENCES file (id) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Contraintes pour la table user_owner_infos
 --
 ALTER TABLE user_owner_infos
-  ADD CONSTRAINT user_owner_infos_ibfk_1 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT user_owner_infos_ibfk_2 FOREIGN KEY (photo_file_id) REFERENCES `file` (id) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT user_owner_infos_ibfk_3 FOREIGN KEY (cv_file_id) REFERENCES `file` (id) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT user_owner_infos_ibfk_1 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT user_owner_infos_ibfk_2 FOREIGN KEY (photo_file_id) REFERENCES file (id) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT user_owner_infos_ibfk_3 FOREIGN KEY (cv_file_id) REFERENCES file (id) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
